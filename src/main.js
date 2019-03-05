3d33c27c829e7924a7810a3714f0eb2b7f410515
wy <wy@126.com>
2019-03-05 21:17:32 +0800

    项目提交代码day7

--- src/main.js
+++ src/main.js
@@ -1,14 +1,24 @@
 
 import Vue from 'vue'
 
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
 import app from './App.vue'
 
+import routerobj from './router.js'
 
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 
 import 'mint-ui/lib/style.css'
-import { Header } from 'mint-ui'
+import { Header, Swipe, SwipeItem  } from 'mint-ui'
 Vue.component(Header.name, Header);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 
 
 
@@ -16,5 +26,6 @@ var vm = new Vue({
     el:'#app',
     render:function(createElements){
         return createElements(app);
-    }
+    },
+    router:routerobj
 })
--- src/router.js
+++ src/router.js
@@ -2,11 +2,25 @@
 
 import VueRouter from 'vue-router'
 
+import home from './components/tabbar/HomeContainer.vue'
+
+import member from './components/tabbar/MemberContainer.vue'
+
+import shopcar from './components/tabbar/ShopcarContainer.vue'
+
+import search from './components/tabbar/SearchContainer.vue'
+
 
 var routerobj = new VueRouter({
     routes:[
-        
-    ]
+        {path:"/",redirect:"/home"},
+        {path:"/home",component: home},
+        {path:"/member",component: member},
+        {path:"/shopcar",component: shopcar},
+        {path:"/search",component: search},
+
+    ],
+    linkActiveClass:'mui-active'
 });
 
 export default routerobj
